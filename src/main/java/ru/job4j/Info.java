f77e9d9c571765ab3810a9813d89358a24f01302
archik <archivestav@gmail.com>
2023-12-30 19:57:57 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class Info {
         public static void main(String[] args) {
-        System.out.println("30.12.2023");
+            System.out.println("30.12.2023");
     }
 }
