b0535a36d79d688c725fab71984aacdf6402221d
archik <archivestav@gmail.com>
2024-01-05 22:30:26 +0300

    7. Расстояние между точками в системе координат [#188 #469849]
    * вывод результатов объединен в одну строку.

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,22 +1,22 @@
 package ru.job4j.condition;
 
-    public class Point {
-        public static double distance(int x1, int y1, int x2, int y2) {
-            double first = x2 - x1;
-            double second = y2 - y1;
-            double three = Math.pow(first, 2);
-            double four = Math.pow(second, 2);
-            double five = three + four;
-            double six = Math.sqrt(five);
-            return six;
-        }
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double first = x2 - x1;
+        double second = y2 - y1;
+        double three = Math.pow(first, 2);
+        double four = Math.pow(second, 2);
+        double five = three + four;
+        double six = Math.sqrt(five);
+        return six;
+    }
 
-        public static void main(String[] args) {
-            double result = Point.distance(0, 0, 2, 0);
-            double result2 = Point.distance(0, 0, 5, 5);
-            double result3 = Point.distance(4, 2, 0, 7);
-            double result4 = Point.distance(1, 3, 6, 8);
-            System.out.println("result (1, 3) to (6, 8), (0, 0) to (2, 0), (0, 0) to (5, 5), (4, 2) to (0, 7)" + result);
+    public static void main(String[] args) {
+        double result = Point.distance(0, 0, 2, 0);
+        double result2 = Point.distance(0, 0, 5, 5);
+        double result3 = Point.distance(4, 2, 0, 7);
+        double result4 = Point.distance(1, 3, 6, 8);
+        System.out.println("result (0, 0), (2, 0) " + result + ", result (0, 0) to (5, 5) " + result2 + ", result (4, 2) to (0, 7) " + result3 + ", result (1, 3) to (6, 8) " + result4);
 
-        }
+    }
 }
